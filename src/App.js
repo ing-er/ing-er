288baf90b9a850603f7083480e7e85fe42d79992
sungyujeon <sungyujeon91@gmail.com>
2021-07-26 10:23:25 +0900

    fix: app.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 import Main from "./pages/Main";
 
 import "./App.css";
+import rootReducer from "./reducers";
+
+const store = createStore(rootReducer);
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Main} />
         </Switch>
       </BrowserRouter>
-    </>
+    </Provider>
   );
 };
 

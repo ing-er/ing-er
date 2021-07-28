e347c46c2e92653acae6ef19bd4982885843b36c
KimHaYoung <ha012345@naver.com>
2021-07-28 14:28:40 +0900

    feat: 버튼 추가

--- src/index.js
+++ src/index.js
@@ -7,12 +7,15 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './modules';
 import { Provider } from 'react-redux';
 
+const HOST = 'localhost:8080';
+const serverUrl = `http://${HOST}/api/v1`;
+
 const store = createStore(rootReducer); // 스토어를 만듭니다.
 console.log(store.getState()); // 스토어의 상태를 확인해봅시다.
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={store} value={{ serverUrl }}>
       <App />
     </Provider>
   </React.StrictMode>,

94adc38e2d93a70179380cb2fd23ec5b82000804
KimHaYoung <ha012345@naver.com>
2021-07-28 15:47:49 +0900

    fix: 투두리스트의 date를 calendar requestdate로 변경

--- src/containers/MyTodolistContainer.js
+++ src/containers/MyTodolistContainer.js
@@ -13,7 +13,7 @@ const MyTodolistComponent = ({ children }) => {
   const dispatch = useDispatch();
   const { todolist } = useSelector((state) => state.setTodolist);
   const { todopercent } = useSelector((state) => state.setTodolist);
-  const { date } = useSelector((state) => state.setCalendar.calendar);
+  const { requestdate } = useSelector((state) => state.setCalendar);
   const setTodolistAddCon = (title) => {
     dispatch(setTodolistAddContainer(title));
   };
@@ -30,18 +30,16 @@ const MyTodolistComponent = ({ children }) => {
     dispatch(setTodolistEditComplete(index, subindex));
   };
 
-  return (
-    cloneElement(children, {
-      todolistdata: todolist,
-      todopercent: todopercent,
-      date: date,
-      setTodolistAddContainer: setTodolistAddCon,
-      setTodolistAddInput: setTodolistAddInp,
-      setTodolistEditTitle: setTodolistEditTi,
-      setTodolistEditContent: setTodolistEditCon,
-      setTodolistEditComplete: setTodolistEditCompl,
-    })
-  );
+  return cloneElement(children, {
+    todolistdata: todolist,
+    todopercent: todopercent,
+    date: requestdate,
+    setTodolistAddContainer: setTodolistAddCon,
+    setTodolistAddInput: setTodolistAddInp,
+    setTodolistEditTitle: setTodolistEditTi,
+    setTodolistEditContent: setTodolistEditCon,
+    setTodolistEditComplete: setTodolistEditCompl,
+  });
 };
 
 export default MyTodolistComponent;

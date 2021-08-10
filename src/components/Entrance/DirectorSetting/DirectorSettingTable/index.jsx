9f4051923c2f3eeb0175c9c0031a480739c1c7e0
sungyujeon <sungyujeon91@gmail.com>
2021-08-10 15:58:06 +0900

    fix: 관리자 페이지 레이아웃 수정

---
+++ src/components/Entrance/DirectorSetting/DirectorSettingTable/index.jsx
@@ -0,0 +1,51 @@
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const createData = (name, calories, fat, carbs) => {
+  return { name, calories, fat, carbs }
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+const DirectorSettingTable = () => {
+  
+  return (
+    <TableContainer component={Paper}>
+      <Table size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell>유형</TableCell>
+            <TableCell align="right">종류</TableCell>
+            <TableCell align="right">수정</TableCell>
+            <TableCell align="right">삭제</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.name}>
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
+
+export default DirectorSettingTable;

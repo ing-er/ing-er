c588dc772b5c27cbc55dbc95ee15e23fab0c9d55
sungyu_jeon <sungyujeon91@gmail.com>
2021-08-16 18:06:55 +0900

    fix: 다짐 일기 레이아웃 수정

--- src/components/Room/DrawerContentContainer/DrawerProfile/RemoteDiary/index.jsx
+++ src/components/Room/DrawerContentContainer/DrawerProfile/RemoteDiary/index.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import { TextField, Grid } from '@material-ui/core';
 import AssignmentIcon from '@material-ui/icons/Assignment';
 
-import { useEffect } from 'react';
+import Wrapper from './styles';
 
 const CalendarDiary = ({ remoteUserInfo }) => {
   const [diary, setDiary] = useState(remoteUserInfo?.diary);
@@ -15,45 +15,47 @@ const CalendarDiary = ({ remoteUserInfo }) => {
   }, [remoteUserInfo])
   
   return (
-    <Grid container direction="column" alignItems="center">
-      <Grid
-        item
-        style={{
-          fontSize: 20,
-          color: '#0E263E',
-        }}
-      >
-        <AssignmentIcon />
-        오늘의 일기
+    <Wrapper>
+      <Grid container direction="column" alignItems="center">
+        <Grid
+          item
+          style={{
+            fontSize: 20,
+            color: '#0E263E',
+          }}
+        >
+          <AssignmentIcon />
+          오늘의 일기
+        </Grid>
+        <Grid
+          item
+          className="textfield-grid"
+          style={{
+            backgroundColor: '#F6F7F9',
+            padding: '20px',
+          }}
+        >
+          {isSecret ? (
+            <TextField 
+              multiline={true}
+              rows={8}
+              fullWidth
+              value={'비공개'}
+              disabled={true}
+            />
+          ) : (
+            <TextField
+              multiline={true}
+              rows={8}
+              fullWidth
+              value={diary ? diary : ''}
+              disabled={true}
+              InputProps={{ disableUnderline: true }}
+            />
+          )}
+        </Grid>
       </Grid>
-      <Grid
-        item
-        className="textfield-grid"
-        style={{
-          backgroundColor: '#F6F7F9',
-          padding: '20px',
-        }}
-      >
-        {isSecret ? (
-          <TextField 
-            multiline={true}
-            rows={8}
-            fullWidth
-            value={'비공개'}
-            disabled={true}
-          />
-        ) : (
-          <TextField
-            multiline={true}
-            rows={8}
-            fullWidth
-            value={diary ? diary : ''}
-            disabled={true}
-            InputProps={{ disableUnderline: true }}
-          />
-        )}
-      </Grid>
-    </Grid>
+    </Wrapper>
   );
 };
 
